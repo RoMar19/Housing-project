0c25dd74088cc47b74e10a60781199dd5c11094d
Romart <rmf1312@hotmail.com>
2020-01-08 12:31:02 +0000

    Updating contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,1 +1,16 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("gmail", "inmadrid_contact_form", 
+    {"from_name": contactForm.name.value,
+    "from_email": contactForm.emailaddress.value,
+    "rentout_apartment": contactForm.summary.value,
+    "typeof_house": contactForm.type.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false; // To block from loading a new page
