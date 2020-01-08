9ac623811842f905791877258ecdf0bef4fe18d7
Romart <rmf1312@hotmail.com>
2020-01-08 12:05:17 +0000

    New contact.js file

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,14 +1,1 @@
- function(){
-      emailjs.init("user_beHlAh5GReL0LXJKxQclh");
-      emailjs.send("gmail", "inmadrid_contact_form", {
-    name: 'Rocio',
-    notes: 'I have an apartment available to rent out ...'
-}; 
-
-
-emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
+console.log("Hello");
