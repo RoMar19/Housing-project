b89bd1300a28f2f7e6697a433600c0563f595890
Romart <rmf1312@hotmail.com>
2020-01-20 14:30:56 +0000

    Adding Json not saved

---
+++ assets/js/json.js
@@ -0,0 +1,37 @@
+{
+"area": {
+"options": [
+"Center",
+"North",
+"South",
+"East",
+"West"
+],
+}
+"type": {
+"options": [
+"Apartment",
+"Private Room",
+"Studio"
+],
+}
+"bedrooms": {
+"options": [
+"1 Bedroom",
+"2 Bedrooms",
+"3 Bedrooms"
+],
+}
+"bathrooms": {
+"options": [
+"1 Bathroom WC/Shower",
+"2 Bathrooims WC/Shower"
+"Private bathroom"
+],
+}
+"rent": {
+"options": [
+"Min. 300 Eur",
+"Max. 1800 Eur",
+]
+},
